17baee8827e5d68217d739288b6d66d395daa412
djnash <djnash@iu.edu>
2025-08-18 08:06:29 -0400

    Add date-fns dependency and implement job listing components
    - Added `date-fns` library for date manipulation.
    - Refactored `HomePage` to include `JobListingItems` component for displaying job listings based on search parameters.
    - Created `JobListingItems` component to fetch and render job listings with relevant details.
    - Introduced utility function `convertSearchParamsToString` for handling search parameters in URL format.
    - Enhanced UI with new components for job listing display and organization.

--- src/app/(job-seeker)/page.tsx
+++ src/app/(job-seeker)/page.tsx
@@ -1,3 +1,13 @@
-export default function HomePage() {
-  return <div>Home Page</div>;
+import { JobListingItems } from '@/components/shared/job-listing-items';
+
+export default function HomePage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | string[]>>;
+}) {
+  return (
+    <div className="m-4">
+      <JobListingItems searchParams={searchParams} />
+    </div>
+  );
 }
